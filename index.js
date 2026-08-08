dc35ed324dc01f642ff4bb893d6218588ffeb338
d3ltaboys <d3ltaboys@proton.me>
2026-08-08 17:49:28 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 <script>
-document.addEventListener("DOMContentLoaded", function () {
-  const footer = document.querySelector("footer");
-  const div = document.createElement("div");
-
-  div.innerHTML = '<a href="https://example.com" target="_blank">متن لینک شما</a>';
-  div.style.textAlign = "center";
-  div.style.padding = "10px";
-
-  if (footer) {
-    footer.insertAdjacentElement("afterend", div);
-  }
-});
+(function () {
+  document.write('<div style="text-align:center;padding:10px;"><a href="https://example.com" target="_blank">متن لینک شما</a></div>');
+})();
 </script>
